4a25942fa19b983732ddf341fdb009f7a0e2f06c
odikil <pedro.donoso.r@protonmail.com>
2022-02-08 10:39:54 -0300

    tercer commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ const express = require('express');
 // Ejecuto el módulo Express y lo guardo en una constante App
 const app = express();
 
-// Middlewares
+// Middlewares: Método para entender formato json
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// routes: App requiere archivo index
+// Routes: App requiere archivo index
 app.use(require('./routes/index'));
 
 // App escucha en el Puerto 3000
 app.listen(3000);
+
 // Cuando escucha envia mensaje
-console
+console.log('Server on port 3000');
